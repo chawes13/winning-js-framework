1f91c5845fa933dbf923ab3317f3ccdcd755f24e
chawes <hawes.conor@gmail.com>
2018-07-19 15:58:59 -0500

    Implemented email + session voting logic

--- server/api/votes.js
+++ server/api/votes.js
@@ -15,16 +15,38 @@ router.get('/', async (req, res, next) => {
     })
     res.json(frameworks)
   } catch (error) {
-    console.error(error)
+    next(error)
   }
 })
 
-// Cast a vote (if haven't already voted)
 router.post('/', async (req, res, next) => {
   try {
-    // TODO: findOrCreate a vote based on email and sessionId
-    res.send('Thanks for voting!')
+    const framework = await Framework.findOne({
+      where: {
+        name: req.body.framework
+      }
+    })
+    if (framework) {
+      const voteOptions = {
+        where: {
+          email: req.body.email,
+          sessionId: req.session.id
+        }
+      }
+      const [vote, created] = await Vote.findOrCreate(voteOptions)
+      if (vote.frameworkId === framework.id) {
+        res.status(200).send('Your vote for this framework has already been recorded')
+      } else {
+        await vote.setFramework(framework)
+        const confirmationMsg = created ? 'Thanks for voting!' : 'Your vote has been updated!'
+        res.status(201).send(confirmationMsg)
+      }
+    } else {
+      const error = new Error('Selected framework is not eligible for voting')
+      error.status(400)
+      next(error)
+    }
   } catch(error) {
-    console.error(error)
+    next(error)
   }
 })
