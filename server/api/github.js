272d8f2dfb24a1b556a39e4dd829413d2a294147
chawes <hawes.conor@gmail.com>
2018-07-20 14:37:32 -0500

    Removed hardcoded date comparison in search query

--- server/api/github.js
+++ server/api/github.js
@@ -6,24 +6,25 @@ const axios = require('axios').create({
     Accept: 'application/vnd.github.v3+json'
   }
 })
+const moment = require('moment')
 module.exports = router
 
-// TODO: Replace hard-coded dates
 router.get('/stats', async (req, res, next) => {
   try {
     const frameworks = await Framework.findAll({
       attributes: ['githubPath']
     })
     const repoAddresses = frameworks.map(framework => framework.githubPath)
+    const lastWeek = moment().subtract(7, 'd').hour(0).format()
 
     const repoPromises = repoAddresses.map(address => {
       return axios.get(`https://api.github.com/repos/${address}`)
     })
     const prPromises = repoAddresses.map(address => {
-      return axios.get(`https://api.github.com/search/issues?q=repo:${address}+type:pr+merged:>=2018-07-13T05:00:00`)
+      return axios.get(`https://api.github.com/search/issues?q=repo:${address}+type:pr+merged:>=${lastWeek}`)
     })
     const issuePromises = repoAddresses.map(address => {
-      return axios.get(`https://api.github.com/search/issues?q=repo:${address}+type:issue+updated:>=2018-07-13T05:00:00`)
+      return axios.get(`https://api.github.com/search/issues?q=repo:${address}+type:issue+updated:>=${lastWeek}`)
     })
 
     const repos = await Promise.all(repoPromises)
