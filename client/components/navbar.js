b1bbe8a9a1a2759e4d0c72b5807ca37ff1a01d86
chawes <hawes.conor@gmail.com>
2018-07-22 18:17:37 -0500

    Adding basic navbar component

---
+++ client/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import { Menu, Icon } from 'semantic-ui-react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <Menu id="navbar" fixed="top" size="huge" inverted borderless >
+        <Menu.Item header>
+          <Icon name="rocket" />
+          JS Framework Activity
+        </Menu.Item>
+      </Menu>
+    </div>
+  )
+}
+
+export default Navbar
