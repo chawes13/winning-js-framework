5598732b3d32c3c64c93a7d4c1ac33263d75166f
chawes <hawes.conor@gmail.com>
2018-07-23 14:35:29 -0500

    Modifying seed to only have framework information

--- script/seed-local.js
+++ script/seed-local.js
@@ -1,7 +1,6 @@
 const db = require('../server/db')
-const { Framework } = require('../server/db/models')
+const { Framework, Vote } = require('../server/db/models')
 
-// Used for initializing the frameworks that are eligible (see seed-local.js for sample votes)
 async function seed() {
   await db.sync({force: true})
   console.log('db synced!')
@@ -13,7 +12,16 @@ async function seed() {
     Framework.create({name: 'Vue', githubPath: 'vuejs/vue'}),
   ])
 
+  const votes = await Promise.all([
+    Vote.create({email: 'react@email.com', sessionId: '1', frameworkId: 1}),
+    Vote.create({email: 'angular@email.com', sessionId: '2', frameworkId: 2}),
+    Vote.create({email: 'ember@email.com', sessionId: '3', frameworkId: 3}),
+    Vote.create({email: 'vue@email.com', sessionId: '4', frameworkId: 1}),
+    Vote.create({email: 'redux@email.com', sessionId: '5', frameworkId: 1}),
+  ])
+
   console.log(`seeded ${frameworks.length} frameworks`)
+  console.log(`seeded ${votes.length} votes`)
   console.log(`seeding completed`)
 }
 
